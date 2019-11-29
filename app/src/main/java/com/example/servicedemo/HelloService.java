bf881f1fbf1df5a8f7e8d680d04e7c7bc40e1872
wolf <wolfag91@gmail.com>
2019-11-29 17:17:16 +0700

    handle open bluetooth

--- app/src/main/java/com/example/servicedemo/HelloService.java
+++ app/src/main/java/com/example/servicedemo/HelloService.java
@@ -5,8 +5,12 @@ import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.Service;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.pm.PackageManager;
 import android.location.Criteria;
 import android.location.Location;
@@ -23,12 +27,38 @@ public class HelloService extends Service {
     private static final String TAG = "HelloService";
     private static final String CHANNEL_ID = "11111";
     private static final String CHANNEL_NAME = "ForegroundServiceChannel";
+    private static final int DELAY_TIME = 5000;
+
+    BluetoothAdapter bluetoothAdapter;
+
+    private final BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (BluetoothAdapter.ACTION_STATE_CHANGED.equals(action)) {
+                final int state = intent.getIntExtra(BluetoothAdapter.EXTRA_STATE, BluetoothAdapter.ERROR);
+                switch (state) {
+                    case BluetoothAdapter.STATE_OFF:
+                        Log.i(TAG, "STATE_OFF");
+                        break;
+                    case BluetoothAdapter.STATE_ON:
+                        Log.i(TAG, "STATE_ON");
+                        break;
+                }
+            }
+        }
+    };
 
     @Override
     public void onCreate() {
         Log.i(TAG, "onCreate");
+
+
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+
         if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
-            NotificationChannel channel=new NotificationChannel(CHANNEL_ID, CHANNEL_NAME, NotificationManager.IMPORTANCE_HIGH);
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, CHANNEL_NAME, NotificationManager.IMPORTANCE_HIGH);
 
             NotificationManager manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
             manager.createNotificationChannel(channel);
@@ -52,10 +82,12 @@ public class HelloService extends Service {
             public void run() {
 
                 Log.i(TAG, "Going" + time);
+                enableBluetooth();
                 time++;
-                handler.postDelayed(this, 1000);
+
+                handler.postDelayed(this, DELAY_TIME);
             }
-        }, 1000);
+        }, DELAY_TIME);
 
         return Service.START_REDELIVER_INTENT;
     }
@@ -71,5 +103,19 @@ public class HelloService extends Service {
     public void onDestroy() {
         Log.i(TAG, "onDestroy");
         Toast.makeText(this, "Service Destroyed", Toast.LENGTH_LONG).show();
+        unregisterReceiver(broadcastReceiver);
+    }
+
+    private void enableBluetooth() {
+        Log.i(TAG, "enableBluetooth");
+        if (bluetoothAdapter == null) {
+            Log.i(TAG, "Does not have BT capabilities");
+        }
+        if (!bluetoothAdapter.isEnabled()) {
+            bluetoothAdapter.enable();
+        }
+
+        IntentFilter intentFilter = new IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED);
+        registerReceiver(broadcastReceiver, intentFilter);
     }
 }
